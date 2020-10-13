1df1a99a48afb57932f82f32063289b2b47021f8
Dillon <Dkernested@hotmail.com>
2020-10-13 16:27:07 -0500

    Creating a home controller and index page at the root of the web app.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    @books = Book.includes(:author)
+                 .order("average_ratings DESC")
+                 .limit(25)
+
+    @authors = Author.limit(10)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  # get 'home/index'
   # get 'authors/index'
   # get 'authors/show'
   # get 'books/index'
   # get 'books/show'
 
+  root to: "home#index"
   resources :books, only: %i[index show]
   resources :authors, only: %i[index show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
