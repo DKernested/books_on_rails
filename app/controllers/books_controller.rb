1066e7d1c5c5c5872f7848a3b394f15b1fa59bf3
Dillon <Dkernested@hotmail.com>
2020-10-13 13:21:33 -0500

    Created view for index and show for author table.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,9 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.includes(:author).order("average_ratings DESC")
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'authors/index'
-  get 'authors/show'
-  get 'books/index'
-  get 'books/show'
+  # get 'authors/index'
+  # get 'authors/show'
+  # get 'books/index'
+  # get 'books/show'
+
+  resources :books, only: %i[index show]
+  resources :authors, only: %i[index show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
