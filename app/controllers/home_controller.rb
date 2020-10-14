add3809d45504f666087f3a68d22dc7232744d3c
Dillon <Dkernested@hotmail.com>
2020-10-14 14:06:06 -0500

    Created footer partial and made the CSV larger.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     @books = Book.includes(:author)
                  .order("average_ratings DESC")
-                 .limit(25)
+                 .limit(10)
 
     @authors = Author.ordered_by_books.limit(10)
   end
