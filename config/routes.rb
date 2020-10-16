d55cfe4dd4f30ee2c5708f03f6c47e26ab8ccbf7
Dillon <Dkernested@hotmail.com>
2020-10-16 02:08:58 -0500

    Added an about page controller, view and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # get 'about/index'
   # get 'home/index'
   # get 'authors/index'
   # get 'authors/show'
@@ -8,5 +9,6 @@ Rails.application.routes.draw do
   root to: "home#index"
   resources :books, only: %i[index show]
   resources :authors, only: %i[index show]
+  resources :about, only: %i[index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
